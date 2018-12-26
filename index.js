03fded92eeb85a62ae709563d206c6227520c8de
Robert Snyder <r.snyder49@gmail.com>
2018-12-26 21:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ class Neighborhood {
     this.id = ++neighborhoodId;
     store.neighborhoods.push(this);
   }
+  deliveries() {
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId === this.id;
+     });
+   }
+   customers() {
+     return store.customers.filter(customer => {
+       return customer.neighborhoodId === this.id;
+     });
+   }
 }
 
 let custormerId = 0;
