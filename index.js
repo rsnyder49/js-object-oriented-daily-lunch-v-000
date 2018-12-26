10bfd1f6a38491fb4f16e9f2bd18edbe722019eb
Robert Snyder <r.snyder49@gmail.com>
2018-12-26 20:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+
+class neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+    store.neighborhood.push(this);
+  }
+}
